e824f2fe9c013b7e3e7d42f42291f422d31f9fe2
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-07-11 02:59:38 -0400

    Bump collections pin to rewritten HEAD

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 dependencies {
     // Simplified Collections
-    api("com.github.simplified-dev:collections") { version { strictly("2f2aa58") } }
+    api("com.github.simplified-dev:collections") { version { strictly("c741e14") } }
 
     // JetBrains Annotations
     api(libs.annotations)
